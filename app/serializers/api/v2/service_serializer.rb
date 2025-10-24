f6004cdd12bebb8ffd3b8d37c50017e4c9256f78
aandrade <aandrade@camsys.com>
2025-10-24 17:44:09 -0400

    [HOPE-252] Access fare details as array in serializer

--- app/serializers/api/v2/service_serializer.rb
+++ app/serializers/api/v2/service_serializer.rb
@@ -27,15 +27,15 @@ module Api
       end
 
       def fare_text
-        object.fare_details.try(:fare_text)
+        object.fare_details[:fare_text]
       end
 
       def fare_url
-        object.fare_details.try(:fare_url)
+        object.fare_details[:fare_url]
       end
 
       def url_partner_text
-        object.fare_details.try(:url_partner_text)
+        object.fare_details[:url_partner_text]
       end
 
     end
