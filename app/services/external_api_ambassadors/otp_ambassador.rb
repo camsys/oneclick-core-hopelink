e9f63069c3fea391aa2da8a2958e9e9e19ece343
aandrade <aandrade@camsys.com>
2025-10-29 17:12:57 -0400

    [HOPE-260] Add null checks to fare_details in otp_ambassador

--- app/services/external_api_ambassadors/otp_ambassador.rb
+++ app/services/external_api_ambassadors/otp_ambassador.rb
@@ -286,9 +286,9 @@ class OTPAmbassador
         leg['serviceFareInfo'] = svc.url
         leg['serviceLogoUrl'] = svc.full_logo_url
         leg['serviceFullLogoUrl'] = svc.full_logo_url(nil)
-        leg['fareText'] = svc.fare_details[:fare_text]
-        leg['fareUrl'] = svc.fare_details[:fare_url]
-        leg['urlPartnerText'] = svc.fare_details[:url_partner_text]
+        leg['fareText'] = svc.fare_details&.dig(:fare_text)
+        leg['fareUrl'] = svc.fare_details&.dig(:fare_url)
+        leg['urlPartnerText'] = svc.fare_details&.dig(:url_partner_text)
       else
         # Fallback to agency information
         agency = leg.dig('route', 'agency')
