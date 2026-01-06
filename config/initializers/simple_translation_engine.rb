378f433773add0d5531ba82d79efc553da08b263
aandrade <aandrade@camsys.com>
2026-01-06 16:01:29 -0500

    [HOPE-263] Override TranslationKeysController to take a return_path param, in order to return the user to the page where the auto translate button was pressed

--- config/initializers/simple_translation_engine.rb
+++ config/initializers/simple_translation_engine.rb
@@ -24,6 +24,49 @@ Rails.configuration.to_prepare do
   TranslationKeysController.class_eval do
     include TranslationsControllerExtensions
     authorize_resource
+
+    def edit(id, return_path = simple_translation_engine.translations_path)
+
+      google_api_key = ENV['GOOGLE_API_KEY']
+
+      translator = (google_api_key ? GoogleTranslator.new(google_api_key) : DummyTranslator.new).from(I18n.default_locale)
+      source_locale = Locale.of(I18n.default_locale)
+
+      @google_translations = {}
+      @return_path = return_path
+
+      Locale.where(name: I18n.available_locales.sort).where.not(name: I18n.default_locale).each do |locale|
+        unless @translation_key.translation(locale)
+          @translation_key.translations.build(locale: locale)
+        end
+
+        unless @translation_key.name.blank?
+          translator = translator.to(locale.name)
+          source_translation = SimpleTranslationEngine.translate(source_locale, @translation_key.name).to_s
+          target_translation = translator.translate(source_translation)
+          @google_translations[locale.id] = target_translation
+        end
+      end
+    end
+
+    def update(id, return_path = simple_translation_engine.translations_path)
+
+      Rails.logger.info "Saving translation.  Params = "
+      Rails.logger.info params
+
+      if @translation_key.update(translation_key_params)
+        flash[:success] = "Translation Successfully Updated"
+        redirect_to return_path
+      else
+        begin
+          @translation_key.update!(translation_key_params)
+        rescue Exception => e
+          Rails.logger.info "Exception saving translation"
+          Rails.logger.info e
+        end
+        render 'edit'
+      end
+    end
   end
 
 
